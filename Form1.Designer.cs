b4095eec2a36a5779a627e226dfe81a309ca9f22
PanovaKate <99796001+PanovaKate@users.noreply.github.com>
2022-04-22 12:06:53 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace Mouse_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MouseDoubleClick = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MouseDoubleClick)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MouseDoubleClick
+            // 
+            this.MouseDoubleClick.Image = ((System.Drawing.Image)(resources.GetObject("MouseDoubleClick.Image")));
+            this.MouseDoubleClick.Location = new System.Drawing.Point(67, 62);
+            this.MouseDoubleClick.Name = "MouseDoubleClick";
+            this.MouseDoubleClick.Size = new System.Drawing.Size(260, 260);
+            this.MouseDoubleClick.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.MouseDoubleClick.TabIndex = 0;
+            this.MouseDoubleClick.TabStop = false;
+           // this.MouseDoubleClick.Click += new System.EventHandler(this.MouseDoubleClick_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MouseDoubleClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.MouseDoubleClick)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MouseDoubleClick;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mouse_1
+{
+    public partial class Form1 : Form
+    {
+        private int MouseDoubleClickWidth = 200, MouseDoubleClickHeight = 200;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MaximumSize = new Size(500, 500);
+            this.MinimumSize = new Size(500, 500);
+            MouseDoubleClick = new PictureBox();
+            MouseDoubleClick.Size = new Size(MouseDoubleClickWidth, MouseDoubleClickHeight);
+
+            // Задаем начальное местоположение PictureBox
+            MouseDoubleClick.Location = new Point(100, 150);
+
+            MouseDoubleClick.BorderStyle = BorderStyle.Fixed3D;
+            this.Controls.Add(MouseDoubleClick);
+           // MouseDoubleClick.MouseDoubleClick += new MouseEventHandler(this.MouseDoubleClick_MouseDoubleClick);
+            MouseDoubleClick.MouseEnter += new EventHandler(this.MouseDoubleClick_MouseEnter);
+            MouseDoubleClick.MouseLeave += new EventHandler(this.MouseDoubleClick_MouseLeave);
+            MouseDoubleClick.MouseDown += new MouseEventHandler(this.MouseDoubleClick_MouseDown);
+            MouseDoubleClick.MouseMove += new MouseEventHandler(this.MouseDoubleClick_MouseMove);
+            MouseDoubleClick.MouseUp += new MouseEventHandler(this.MouseDoubleClick_MouseUp);
+
+            this.MouseWheel += new MouseEventHandler(MouseDoubleClick_MouseWheel);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            int x_mouse = e.X;
+            int y_mouse = e.Y;
+            MessageBox.Show("Координаты мыши внутри PictureBox" + x_mouse + " " + y_mouse);
+        }
+        private void MouseDoubleClick_MouseEnter(object sender, EventArgs e)
+        {
+            // mouse.BackColor = Color.CadetBlue;
+            MouseDoubleClick.Image = Image.FromFile(@"C:\Downloads\mouse2.png");
+            MouseDoubleClick.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+        private void MouseDoubleClick_MouseLeave(object sender, EventArgs e)
+        {
+            // mouse.BackColor = Color.AliceBlue;
+            MouseDoubleClick.Image = Image.FromFile(@"C:\Downloads\mouse1.png");
+            MouseDoubleClick.SizeMode = PictureBoxSizeMode.StretchImage;
+
+        }
+        private bool isDragging = false; 
+        private int oldX, oldY;
+        private void MouseDoubleClick_MouseDown(object sender, MouseEventArgs e)
+        {
+            Cursor.Current = Cursors.Hand;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = true;
+                oldX = e.X;
+                oldY = e.Y;
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                //MessageBox.Show(" I'm really good! ");
+                ToolTip podskazka = new ToolTip();
+                podskazka.SetToolTip(MouseDoubleClick, "I'm really good!\nGood job!\nGood work!\nWay to go!");
+            }
+        }
+
+        private void MouseDoubleClick_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                MouseDoubleClick.Top = MouseDoubleClick.Top + (e.Y - oldY); //координаты PictureBox
+                MouseDoubleClick.Left = MouseDoubleClick.Left + (e.X - oldX); //координаты PictureBox
+            }
+        }
+        private void MouseDoubleClick_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+
+        }
+        private double ImageScale = 1.0;
+        private void MouseDoubleClick_MouseWheel(object sender, MouseEventArgs e)
+        {
+            // изменение размера при прокручивании колесика мышки
+
+            const double scale_per_delta = 0.1 / 120;
+            // пересчитываем размер
+            ImageScale += e.Delta * scale_per_delta;
+
+            // чтобы не выйти за разумные размеры
+            if (ImageScale < 0.5) ImageScale = 0.5;
+
+            if (ImageScale > 1.5) ImageScale = 1.5f;
+
+            // устанавливаем размер PictureBox
+
+            MouseDoubleClick.Size = new Size((int)(MouseDoubleClickWidth * ImageScale), (int)(MouseDoubleClickHeight * ImageScale));
+
+        }
+
+    }
+}
